869bc1a3411fb4ce83427efcf24681b7ef41ca33
AYUSH-PATHAK-1 <developerasp7@gmail.com>
2023-07-29 10:26:48 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [darkmode, setDarkmode] = useState({
+    color: "#000",
+    backgroundColor: "#fff"
+  });
+
+  const changecolors=()=>{
+    if(darkmode.color==="#000"){
+    setDarkmode({
+      color:"#fff",
+      backgroundColor:"#000"
+    })
+    SetText("Light Mode");
+  }else{
+    setDarkmode({
+      color:"#000",
+      backgroundColor:"#fff"
+    })
+    SetText("Dark Mode");
+  }}
+  // const changeback=()=>{
+  //   setDarkmode({
+  //     color:"#000",
+  //     backgroundColor:"#fff"
+  //   })
+  // }
+
+  const [text,SetText]=useState("Dark Mode")
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid text-center" style={darkmode}>
+      <button className="btn btn-outline-danger my-4" onClick={changecolors} >{text}</button>
+      <div className="jumbotron text-center" >
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      </div>
     </div>
   );
 }
